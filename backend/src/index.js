8a912cb4d733c20101b632939e4d8eb8113e603e
Andrey Gonçalves <49600701+zAndreyG@users.noreply.github.com>
2020-04-02 19:37:57 -0300

    Dev - Entity

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,5 +13,3 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
-/* Start in Backend: 51:30m */
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,8 @@
 const { Router } = require('express');
+const DevController = require('./controller/DevController');
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Oi Mundo!' });
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
