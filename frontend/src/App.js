5757fd7e59eeacf9ed8ed1906475d84a176d7072
Andrey Gonçalves <49600701+zAndreyG@users.noreply.github.com>
2020-04-13 19:40:33 -0300

    Organizing frontend app structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,33 +6,15 @@ import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 // Componente --> Função que retorna alguma coisa - Bloco isolado de HTML, CSS, e JS que não interfere no restante da aplicação
 // Propiedade --> Propriedades da função - O mesmo que os atributos do HTML - Informações que um componente PAI passa ao componente FILHO
 // Estado     --> Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
   const [devs, setDevs] = useState([]);
-
-  const [github_username, setGithubUsernames] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } =position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-
-      },
-      {
-        timeout: 30000,
-      }
-    )
-  }, []);
 
   useEffect(() => {
     async function loadDevs() {
@@ -44,20 +26,8 @@ function App() {
     loadDevs();
   }, []);
 
-  
-
-  async function handleAddDev(e) {
-    e.preventDefault();
-    
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      longitude,
-      latitude,
-    })
-
-    setGithubUsernames('');
-    setTechs('');
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
 
     setDevs([...devs, response.data]);
   }
@@ -66,75 +36,15 @@ function App() {
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input 
-              type="text" 
-              name="github_username" 
-              id="github_username" 
-              required
-              value={github_username}
-              onChange={e => setGithubUsernames(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-              type="text" 
-              name="techs" 
-              id="techs" 
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                name="latitude" 
-                id="latitude" 
-                required 
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                name="longitude" 
-                id="longitude" 
-                required 
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubimit={handleAddDev} />
+        
       </aside>
 
       <main>
         <ul>
           {devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img src={dev.avatar_url} alt={dev.name}/>
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-              <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
-            </li>
+            <DevItem key={dev._id} dev={dev} />
           ))}
-          
         </ul>
       </main>
     </div>
@@ -142,6 +52,3 @@ function App() {
 }
 
 export default App;
-
-
-/* Start in 1:18:58Hs */
