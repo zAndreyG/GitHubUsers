cc31f99a2b12084c3d2cc1766158c18fa7e4fe79
Andrey Gonçalves <49600701+zAndreyG@users.noreply.github.com>
2020-04-15 19:56:50 -0300

    Add Pages + Config Routes

---
+++ mobile/src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Main from './pages/Main';
+import Profile from './pages/Profile';
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{
+                headerShown: true,
+                headerStyle: {
+                    backgroundColor: '#7D40E7'
+                },
+                headerTintColor: '#FFF',
+            }}>
+                <AppStack.Screen name="DevRadar" component={Main} />
+                <AppStack.Screen name="Perfil" component={Profile} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
